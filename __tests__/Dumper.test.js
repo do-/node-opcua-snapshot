247b4aa7453be19d2cc419300c853451ece103b9
Dmitry Ovsyanko <dovsyanko@cheby.ru>
2026-01-07 07:33:08 +0300

    Dumper.js 100% coverage

--- __tests__/Dumper.test.js
+++ __tests__/Dumper.test.js
@@ -1,4 +1,5 @@
-const {OPCUAClient, OPCUAServer} = require ('node-opcua')
+const {OPCUAClient, OPCUAServer, ReferenceTypeIds, NodeClass} = require ('node-opcua')
+
 const Dumper = require ('../lib/Dumper')
 const dump = require ('./data/dump.json')
 const init = require ('./lib/init')
@@ -31,18 +32,79 @@ describe ('Dumper', () => {
     await server.shutdown ()
   })  
 
-  describe ('dump', () => {
+  describe ('dump', () => {    
     
     it ('should fetch the equivalent of the pre recorded dump', async () => {
+
       const snapshot = await dumper.dump ({ns: 1, id: 'i=5001'})
+
       expect (snapshot.Organizes [0]).toEqual (dump)
+
+    })
+
+    it ('should fetch a leaf object because contained node types are not yet supported', async () => {
+
+      const snapshot = await dumper.dump ({ns: 0, id: 'i=24'})
+
+      expect (snapshot).toEqual ({
+        class: 'Object',
+        ns: 0,
+        id: 'i=24',
+        type: 'FolderType'
+      })
+
     })
 
     it ('should carp and find nothing', async () => {
+
       const a = []; dumper.on ('warning', _ => a.push (String (_)))
+
       const snapshot = await dumper.dump ({ns: 1, id: 'i=5002'})
+
       expect (snapshot.Organizes).toBeUndefined ()
+
       expect (a).toEqual (['BadNodeIdUnknown (0x80340000)'])
+
+    })
+
+  })
+
+  describe ('ingoring unknown things', () => {    
+
+    it ('should cover the non-`HasComponent` nor `Organizes` case for a reference', async () => {
+
+      for (const referenceTypeId in ReferenceTypeIds) if (referenceTypeId !== 'HasComponent' && referenceTypeId !== 'Organizes') {
+
+        const child = await dumper.loadReference (referenceTypeId, 1, 'ns=1;i=5001', '?', 'Variable')
+
+        expect (child).toBeUndefined ()
+
+      }
+
+    })
+
+    it ('should cover the non-`Variable` nor `Method` case for the `HasComponent` type reference', async () => {
+
+      for (const nodeClass in NodeClass) if (nodeClass !== 'Method' && nodeClass !== 'Variable') {
+
+        const child = await dumper.loadReferenceHasComponent ({}, nodeClass)
+
+        expect (child).toBeUndefined ()
+
+      }
+
+    })
+
+    it ('should cover the non-`Object` case for the `Organizes` type reference', async () => {
+
+      for (const nodeClass in NodeClass) if (nodeClass !== 'Object') {
+
+        const child = await dumper.loadReferenceOrganizes ({}, nodeClass)
+
+        expect (child).toBeUndefined ()
+
+      }
+
     })
 
   })
